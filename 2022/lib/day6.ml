99d891618fc8e46f7ae5f3f1e520cc1bd45d2699
unknown <firgeis@gmail.com>
2022-12-13 02:57:08 -0300

    Better Day6

--- 2022/lib/day6.ml
+++ 2022/lib/day6.ml
@@ -1,35 +1,27 @@
 module CharSet = Set.Make(Char)
-let add_char list c quantity =
-  if List.length list < quantity then
-    c::list
-  else
-    c::list |> List.rev |> List.tl |> List.rev
 
 let is_signal list quantity =
-  let charset = CharSet.of_list list in
+  let charset = CharSet.of_list (list |> Array.to_list) in
   CharSet.cardinal charset = quantity
 
-let search_signal window chars quantity = 
-  let rec aux window chars idx =
+let search_signal chars quantity = 
+  let rec aux window idx =
     if is_signal window quantity then
-      idx
+      idx - 1 + quantity
     else
-      match chars with
-      | [] -> assert false
-      | h::t -> 
-        aux (add_char window h quantity) t (idx + 1)
+      aux (Array.sub chars idx quantity) (idx + 1)
   in
-  aux window chars 0
+  aux (Array.sub chars 0 quantity) 0
 
 let radio signal_size =
   let chars =
     Helpers.lines "day6.txt"
     |> Seq.uncons
     |> fun input -> Option.get input |> fst |> String.to_seq
-    |> List.of_seq
+    |> Array.of_seq
   in
 
-  search_signal [] chars signal_size
+  search_signal chars signal_size
 
 let result1 =
   radio 4
