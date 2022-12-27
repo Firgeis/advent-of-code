86af1909c1d1f40266135c28d59d70721a1baacd
unknown <firgeis@gmail.com>
2022-12-27 02:46:15 -0300

    Day8

--- 2022/bin/main.ml
+++ 2022/bin/main.ml
@@ -1,2 +1,2 @@
 Printexc.record_backtrace true;
-Printf.printf "\n%i\n" Aoc2022.Day8.result1;
+Printf.printf "\n%i\n" Aoc2022.Day8.result2;
--- 2022/lib/day8.ml
+++ 2022/lib/day8.ml
@@ -4,6 +4,13 @@ type direction =
 | Top
 | Bottom
 
+let print_matrix m name =
+  print_endline name;
+  m
+  |> Array.iter (fun x -> Array.iter (fun y -> Printf.printf "%i" y) x; print_endline "";);
+  print_endline "";
+  ()
+
 let sweep forest =
   let maxy = (Array.length forest - 1) in
   let maxx = (Array.length (Array.get forest 0) - 1) in
@@ -18,13 +25,13 @@ let sweep forest =
     match direction with
     | Left ->
       (match index, indey with
-      | x, y when x < maxx -> Printf.printf "\n%i" x; aux (x + 1) y direction setmaxline new_arr
-      | x, y when x = maxx && y < maxy -> Printf.printf "A\n%i" x; aux 0 (y + 1) direction 0 new_arr
+      | x, y when x < maxx -> aux (x + 1) y direction setmaxline new_arr
+      | x, y when x = maxx && y < maxy -> aux 0 (y + 1) direction 0 new_arr
       | _ -> new_arr)
     | Right -> 
       (match index, indey with
-      | x, y when x < maxx -> aux (x - 1) y direction setmaxline new_arr
-      | x, y when x = maxx && y < maxy -> aux maxx (y + 1) direction 0 new_arr
+      | x, y when x > 0 -> aux (x - 1) y direction setmaxline new_arr
+      | x, y when x = 0 && y < maxy -> aux maxx (y + 1) direction 0 new_arr
       | _ -> new_arr)
     | Top ->
       (match index, indey with
@@ -33,33 +40,105 @@ let sweep forest =
       | _ -> new_arr)
     | Bottom ->
       (match index, indey with
-      | x, y when y < maxy -> aux x (y - 1) direction setmaxline new_arr
-      | x, y when y = maxy && x < maxx -> aux (x + 1) maxy direction 0 new_arr
+      | x, y when y > 0 -> aux x (y - 1) direction setmaxline new_arr
+      | x, y when y = 0 && x < maxx -> aux (x + 1) maxy direction 0 new_arr
       | _ -> new_arr)
   in
+  let length_x = maxx + 1 in
+  let length_y = maxy + 1 in
+  let left = aux 0 0 Left 0 (Array.make_matrix length_x length_y 0) in
+  let right = aux maxx 0 Right 0 (Array.make_matrix length_x length_y 0) in
+  let top = aux 0 0 Top 0 (Array.make_matrix length_x length_y 0) in
+  let bottom = aux 0 maxy Bottom 0 (Array.make_matrix length_x length_y 0) in
 
-  let left = aux 0 0 Left 0 (Array.make_matrix maxx maxy 0) in
-  let right = aux maxx maxy Right 0 (Array.make_matrix maxx maxy 0) in
-  let top = aux 0 0 Top 0 (Array.make_matrix maxx maxy 0) in
-  let bottom = aux maxx maxy Bottom 0 (Array.make_matrix maxx maxy 0) in
-
-  let result = Array.make_matrix maxx maxy 0 in
+  let result = Array.make_matrix length_x length_y 0 in
 
   Array.iteri (
     fun y ys ->
       Array.iteri (fun x _ ->
-        result.(y).(x) <- left.(y).(x) * right.(y).(x) * top.(y).(x) * bottom.(y).(x))
+        if x = 0 || y = 0 || x = maxx || y = maxy then
+          result.(y).(x) <- 1
+        else
+          result.(y).(x) <- left.(y).(x) + right.(y).(x) + top.(y).(x) + bottom.(y).(x))
     ys) result;
 
   result
-  |> Array.fold_left (fun acc x -> acc + Array.fold_left (fun accc xx -> accc + xx) 0 x) 0
+  |> Array.fold_left (fun acc x -> acc + Array.fold_left (fun accc xx -> if xx = 0 then accc + 1 else accc) 0 x) 0
+  |> fun x -> (length_x * length_y) - x
+
+let direction_score x y maxx maxy forest direction =
+  let current = forest.(y).(x) in
+  match direction with
+  | Left ->
+    Seq.unfold (fun x -> 
+      if not (x = -1) then 
+        if (forest.(y).(x)) >= current then 
+          Some (forest.(y).(x), -1)
+        else 
+          Some (forest.(y).(x),(x - 1))
+      else 
+        None) (x - 1)
+    |> fun s -> max (Seq.length s) 1
+  | Right ->
+    Seq.unfold (fun x -> 
+      if not (x > maxx) then 
+        if (forest.(y).(x)) >= current then 
+          Some (forest.(y).(x), maxx + 1)
+        else 
+          Some (forest.(y).(x),(x + 1))
+      else
+        None) (x + 1)
+    |> fun s -> max (Seq.length s) 1
+  | Top ->
+    Seq.unfold (fun y -> 
+      if not (y = -1) then 
+        if (forest.(y).(x)) >= current then
+          Some (forest.(y).(x), -1)
+        else 
+          Some (forest.(y).(x),(y - 1))
+      else
+        None) (y - 1)
+    |> fun s -> max (Seq.length s) 1
+  | Bottom ->
+    Seq.unfold (fun y -> 
+      if not (y > maxy) then 
+        if (forest.(y).(x)) >= current then 
+          Some (forest.(y).(x), maxy + 1) 
+        else 
+          Some (forest.(y).(x),(y + 1)) 
+      else 
+        None) (y + 1)
+    |> fun s -> max (Seq.length s) 1
+
+
+let find_scenic_spot forest =
+  let maxy = (Array.length forest - 1) in
+  let maxx = (Array.length (Array.get forest 0) - 1) in
+  let max = ref 0 in
+  print_endline "";
+  for y = 1 to (maxy - 1) do
+    for x = 1 to (maxx - 1) do
+      let this_direction_score = direction_score x y maxx maxy forest in
+      let left = this_direction_score Left in
+      let right = this_direction_score Right in
+      let top = this_direction_score Top in
+      let bottom = this_direction_score Bottom in
+      let scenic_score = left * right * top * bottom in
+      Printf.printf "%i" scenic_score;
+      if scenic_score > !max then max := scenic_score;
+    done;
+    print_endline "";
+  done;
+  !max
+
+let forest =
+  Helpers.lines "day8.txt"
+  |> Seq.map (fun line -> line |> String.to_seq |> Seq.map (fun c -> int_of_char c - 48) |> Array.of_seq)
+  |> Array.of_seq
+
 
 let result1 =
-  Printf.printf "%s" (Sys.getcwd ());
-  let forest =
-    Helpers.lines "day8.txt"
-    |> Seq.map (fun line -> line |> String.to_seq |> Seq.map (fun c -> int_of_char c - 48) |> Array.of_seq)
-    |> Array.of_seq
-  in
-
   sweep forest
+
+let result2 =
+  find_scenic_spot forest
